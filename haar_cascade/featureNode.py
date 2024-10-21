b09fbc497cd16581b9031fb0e215cd0859bd747f
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2024-10-21 18:24:56 +0700

    Haar

--- haar_cascade/featureNode.py
+++ haar_cascade/featureNode.py
@@ -4,16 +4,7 @@ class FeatureType(Enum):
     NOFEATURE = 0
     SIMPLE1 = 1
     SIMPLE2 = 2
-    SIMPLE3 = 3
-    SIMPLE4 = 4
-    SIMPLE5 = 5
-    SIMPLE6 = 6
-    SIMPLE7 = 7
-    SIMPLE8 = 8
-    SIMPLE9 = 9
-    SIMPLE10 = 10
-    SIMPLE11 = 11
-    SIMPLE12 = 12
+
 
 class FeatureNode:
     type : FeatureType
