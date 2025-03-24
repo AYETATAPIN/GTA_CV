85f692055d44183dbc1e1d5b6f54075f7e162348
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-03-24 13:58:21 +0700

    Some mistakes in dataLoader

--- VAE/myDataLoader.py
+++ VAE/myDataLoader.py
@@ -9,9 +9,10 @@ class myDataLoader:
 
 
     def getNext(self):
-        if self.value >= self.max:
+        if self.count >= self.max:
             return None
         else:
             value = torchvision.io.read_image(self.path + str(self.count) + '.jpg')
             self.count += 1
+            value = value.unsqueeze(0)
             return value
