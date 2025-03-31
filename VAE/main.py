e77751939d2d23b618b01ec9bbf852e2366777bf
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-03-31 17:22:27 +0700

    Restructed and tested training

--- VAE/main.py
+++ VAE/main.py
@@ -4,100 +4,117 @@ import torch.optim
 
 from myDataLoader import myDataLoader
 
+startCh = 3
 hidden_dim = 20
-cnt_epochs = 100
+cnt_epochs = 1000
 max_photo = 1000
 path_to_photo = ""
 path_to_save = 'D:\Documents\Pycharm Projects\VAE'
 name_of_model = 'model'
 
+device = 'gpu' if torch.cuda.is_available() else 'cpu'
+
 class Vae(nn.Module):
-    def __init__(self, hiddenShapes: [int] = None):
+    def __init__(self):
         super(Vae, self).__init__()
 
-    
+
+        self.encInChannels = startCh
+        self.encOutChannels = 512
 
 
-        modules = []
-        if hiddenShapes is None:
-            hiddenShapes = [32, 64, 128, 256]
 
-        self.encInChannels = 1
-        self.encOutChannels = hiddenShapes[-1]
+        self.ec1 = nn.Conv2d(3, 32, 3, stride=2, padding=1)
+        self.ec1A = nn.ReLU()
+        self.ec2 = nn.Conv2d(32, 64, 3, stride=2, padding=1)
+        self.ec2A = nn.ReLU()
+        self.ec3 = nn.Conv2d(64, 128, 3, stride=2, padding=1)
+        self.ec3A = nn.ReLU()
+        self.ec4 = nn.Conv2d(128, 256, 3, stride=2, padding=1)
+        self.ec4A = nn.ReLU()
+        self.ec5 = nn.Conv2d(256, 512, 3, stride=2, padding=1)
+        self.ec5A = nn.ReLU()
+
+        self.encFlatten = nn.Flatten(start_dim=1)
+
+        self.enc_mu = nn.Linear(512*16*8, hidden_dim)
+        self.enc_var = nn.Linear(512*16*8, hidden_dim)
 
 
-        start = 1
-        for outCh in hiddenShapes:
-            modules.append(
-                nn.Sequential(
-                    nn.Conv2d(start, outCh, 3, stride=2, padding=1),
-                    nn.ReLU()
-                )
-            )
-            start = outCh
 
-        self.encoder =  nn.Sequential(*modules)
-        self.enc_mu = nn.Linear(hiddenShapes[-1] * 4, hidden_dim)
-        self.enc_var = nn.Linear(hiddenShapes[-1] * 4, hidden_dim)
+        self.decoder_in = nn.Linear(hidden_dim, 512*8*16)
 
-        hiddenShapes.reverse()
-        hiddenShapes.append(1)
+        self.dc1 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.dc1A = nn.ReLU()
+        self.dc2 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.dc2A = nn.ReLU()
+        self.dc3 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.dc3A = nn.ReLU()
+        self.dc4 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.dc4A = nn.ReLU()
+        self.dc5 = nn.ConvTranspose2d(32, startCh, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.dc5A = nn.ReLU()
 
-        modules = []
-        self.decoder_in = nn.Linear(hidden_dim, hiddenShapes[0]*4)
-
-        for i in range(len(hiddenShapes) - 1):
-            modules.append(
-                nn.Sequential(
-                    nn.ConvTranspose2d(hiddenShapes[i], hiddenShapes[i+1],
-                                       kernel_size = 3, stride= 2, padding=1),
-                    nn.ReLU()
-                )
-            )
-
-        self.decoder = nn.Sequential(*modules)
 
 
     def encode(self, x):
-        z = self.encoder(x)
-        return self.enc_mu(z), self.enc_var(z)
+        x = self.ec1A(self.ec1(x))
+        x = self.ec2A(self.ec2(x))
+        x = self.ec3A(self.ec3(x))
+        x = self.ec4A(self.ec4(x))
+        x = self.ec5A(self.ec5(x))
+        x = self.encFlatten(x)
+        return self.enc_mu(x), self.enc_var(x)
 
     def reparam(self, mu, var):
-        std = torch.exp(0.5*var)
+        std = torch.exp(0.5 * var)
         eps = torch.randn_like(std)
         return mu + eps * std
 
     def decode(self, z):
-        return self.decoder(z)
+        z = self.decoder_in(z)
+        z = z.view(1, self.encOutChannels, 16, 8)
+        z = self.dc1A(self.dc1(z))
+        z = self.dc2A(self.dc2(z))
+        z = self.dc3A(self.dc3(z))
+        z = self.dc4A(self.dc4(z))
+        z = self.dc5A(self.dc5(z))
+        return z
 
     def forward(self, x):
         mu, var = self.encode(x)
         z = self.reparam(mu, var)
-        return self.decode(z)
+        return self.decode(z), mu, var
 
-model = Vae()
+
+model = Vae().to(device)
 optim = torch.optim.Adam(model.parameters())
 
+
 def myLoss(myx, x, mu, var):
-    RecLoss = nn.functional.binary_cross_entropy(myx, x)
+    RecLoss = nn.functional.mse_loss(myx, x)
     DKL = -0.5 * torch.sum(1 + var - mu.pow(2) - var.exp())
 
     return RecLoss + DKL
 
+
 def train():
     loader = myDataLoader(max_photo, path_to_photo)
-    img = loader.getNext()
+    img = loader.getNext().to(device)
     while img is not None:
         optim.zero_grad()
         myImg, mu, var = model.forward(img)
         losses = myLoss(myImg, img, mu, var)
         losses.backward()
         optim.step()
+        img = loader.getNext().to(device)
+
 
 def mainTrain(epochs):
     model.train()
     for i in range(epochs):
         train()
+
 
 if __name__ == "__main__":
     mainTrain(cnt_epochs)
--- VAE/myDataLoader.py
+++ VAE/myDataLoader.py
@@ -1,10 +1,10 @@
-
+import torch
 import torchvision
 
 class myDataLoader:
-    def __init__(self, max, path):
+    def __init__(self, maxImg, path):
         self.count = 0
-        self.max = max
+        self.max = maxImg
         self.path = path
 
 
@@ -14,5 +14,11 @@ class myDataLoader:
         else:
             value = torchvision.io.read_image(self.path + str(self.count) + '.jpg')
             self.count += 1
+            tr = torchvision.transforms.Resize(size= (512, 256))
+            value = tr(value)
+
+            value = value.to(torch.float32)
+
             value = value.unsqueeze(0)
+
             return value
