05ec4580b4c9f2ac01f11772ddfd015c8da580f3
Dimandr <Dmitry221556@yandex.ru>
2025-04-11 11:27:47 +0700

    ы запретил аборты дополнил работу с бд

--- ResultServer.py
+++ ResultServer.py
@@ -1,23 +1,22 @@
-import sqlite3
 import cv2 as cv
 import numpy as np
 import base64
 import datetime
 import os
-from flask import Flask, request, jsonify, abort
-
+from flask import Flask, request, jsonify
+import DB_managing as db
 app = Flask(__name__)
 images_folder = 'coded_images'
 
 
-def save_image(image, license_plate, date_time):
+def save_photo(image, license_plate, date_time):
     coded_image = vae_coding(image)
     image_path = os.path.join(images_folder, license_plate + date_time + '.bin')
 
     with open(image_path, 'wb') as file:
         file.write(coded_image)
 
-    save_to_db(image_path, license_plate, date_time)
+    db.save_photo_to_db(image_path, license_plate, date_time)
 
 
 # 2 заглушки пока нет реального vae
@@ -31,29 +30,10 @@ def vae_decoding(coded_image):
     return decoded_image
 
 
-def save_to_db(image_path, license_plate, date_time):  # str, str, str
-    connection = sqlite3.connect('cars_v0.db')
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO Cars (image_path, license_plate, date_time) VALUES (?, ?, ?)',
-                   (image_path, license_plate, date_time))
-    connection.commit()
-    connection.close()
-
-
-def get_images_paths_from_db(license_plate):
-    connection = sqlite3.connect('cars_v0.db')
-    cursor = connection.cursor()
-    cursor.execute('SELECT image_path FROM Cars WHERE license_plate = ?', (license_plate,))
-    paths = cursor.fetchall()
-    connection.close()
-    result = tuple(element[0] for element in paths)
-    return result
 
 
 def get_images(license_plate):
-    paths = get_images_paths_from_db(license_plate)
-    if len(paths) == 0:
-        abort(404)
+    paths = db.get_images_paths_from_db(license_plate)
     images = []
     for path in paths:
         with open(path, 'rb') as file:
@@ -78,21 +58,35 @@ def greeting():
   Используйте /<license_plate> GET, чтобы получить изображения по указанному номеру.'''
 
 
-@app.route('/save', methods=['POST'])
-def save_request():
+@app.route('/savephoto', methods=['POST'])
+def save_photo_request():
     license_plate = request.form.get('license_plate')
     date_time = request.form.get('date_time')
 
     image_file = request.files['image']
     if (license_plate == ""
             or image_file.filename == "" or not is_correct_datetime(date_time)):
-        abort(400)
+        return 'wrong request', 400
 
     image_array = np.frombuffer(image_file.read(), np.uint8)
 
     image = cv.imdecode(image_array, cv.IMREAD_COLOR)
-    save_image(image, license_plate, date_time)
+    save_photo(image, license_plate, date_time)
     return "Success" + "| license-plate: " + license_plate + "| date-time: " + date_time
+
+
+@app.route('/savecar', methods=['POST'])
+def save_car_request():
+    license_plate = request.form.get('license_plate')
+    model = request.form.get('model')
+    owner = request.form.get('owner')
+
+    if (license_plate == ""):
+        return 'wrong request', 400
+
+
+    db.save_car_to_db(license_plate, model, owner)
+    return "Success" + "| license-plate: " + license_plate
 
 
 def send_images(images):
@@ -100,7 +94,7 @@ def send_images(images):
     for image in images:
         res, coded_image = cv.imencode('.jpg', image)
         if not res:
-            abort(500)
+            return 'cv.imencode error', 500
         result.append(base64.b64encode(coded_image).decode('utf-8'))
     return jsonify(result)
 
@@ -108,23 +102,11 @@ def send_images(images):
 @app.route('/<license_plate>')
 def get_by_license_plate_request(license_plate):
     images = get_images(license_plate)
+    if len(images) == 0:
+        return 'images not found', 404
     return send_images(images)
 
 
 if __name__ == "__main__":
-    # Удостоверились, что с базой всё в порядке
-    connection = sqlite3.connect('cars_v0.db')
-    cursor = connection.cursor()
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Cars (
-    license_plate TEXT NOT NULL,
-    date_time TEXT NOT NULL,
-    image_path TEXT NOT NULL,
-    PRIMARY KEY (license_plate, date_time)
-    )''')
-    #
-    # date_time хранится в формате Year.Month.Date Hour:Min:Sec
-    connection.commit()
-    connection.close()
-
+    db.start_validation()
     app.run(port=50505)
