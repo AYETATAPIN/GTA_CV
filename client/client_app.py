deae78f3671594171ea170052d7e16cccf9395ba
ErzhanKorzhan <r.badyrtenov1@g.nsu.ru>
2025-04-15 01:40:35 +0700

    all

--- client/client_app.py
+++ client/client_app.py
@@ -1,39 +1,11 @@
 from flask import Flask, render_template, request
 import requests
-from io import BytesIO
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/save', methods=['POST'])
-def handle_save():
-    license_plate = request.form.get('license_plate')
-    date_time = request.form.get('date_time')
-    image = request.files['image']
-
-    if not license_plate or not date_time or not image:
-        return "Все поля обязательны для заполнения", 400
-
-    try:
-        image_data = image.read()
-        files = {'image': (image.filename, BytesIO(image_data), image.content_type)}
-        data = {'license_plate': license_plate, 'date_time': date_time}
-
-        response = requests.post(
-            'http://localhost:50505/save',
-            data=data,
-            files=files
-        )
-        response.raise_for_status()
-        return response.text
-    except Exception as e:
-        return f"Ошибка: {str(e)}", 500
-
 
 @app.route('/display')
 def display():
