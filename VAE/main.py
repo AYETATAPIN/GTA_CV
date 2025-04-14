22ba9adb8456441ca9212f98a15055f269b0d18a
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-04-14 18:16:39 +0700

    Vae

--- VAE/main.py
+++ VAE/main.py
@@ -6,12 +6,13 @@ from torch import Tensor
 from myDataLoader import myDataSet
 
 startCh = 3
-hidden_dim = 1000
+hidden_dim = 128
 cnt_epochs = 100
 path_to_photo = "D:\\Documents\\Pycharm Projects\\Vae2\\cars\\cars"
 path_to_save = 'D:\\Documents\\Pycharm Projects\\Vae2'
 name_of_model = 'model'
 is_my_KLD = False
+batch_size = 4
 
 
 device = 'gpu' if torch.cuda.is_available() else 'cpu'
@@ -26,48 +27,48 @@ class Vae(nn.Module):
 
 
 
-        self.ec1 = nn.Conv2d(3, 32, 3, stride=2, padding=1)
+        self.ec1 = nn.Conv2d(3, 32, 3, stride=2, padding=1, dtype=torch.float16)
         self.ec1A = nn.LeakyReLU()
-        self.ec2 = nn.Conv2d(32, 64, 3, stride=2, padding=1)
+        self.ec2 = nn.Conv2d(32, 64, 3, stride=2, padding=1, dtype=torch.float16)
         self.ec2A = nn.LeakyReLU()
-        self.ec3 = nn.Conv2d(64, 128, 3, stride=2, padding=1)
+        self.ec3 = nn.Conv2d(64, 128, 3, stride=2, padding=1, dtype=torch.float16)
         self.ec3A = nn.LeakyReLU()
-        self.ec4 = nn.Conv2d(128, 256, 3, stride=2, padding=1)
+        self.ec4 = nn.Conv2d(128, 256, 3, stride=2, padding=1, dtype=torch.float16)
         self.ec4A = nn.LeakyReLU()
-        self.ec5 = nn.Conv2d(256, 512, 3, stride=2, padding=1)
+        self.ec5 = nn.Conv2d(256, 512, 3, stride=2, padding=1, dtype=torch.float16)
         self.ec5A = nn.LeakyReLU()
-        self.ec6 = nn.Conv2d(512, 1024, 3, stride=2, padding=1)
+        self.ec6 = nn.Conv2d(512, 1024, 3, stride=2, padding=1, dtype=torch.float16)
         self.ec6A = nn.LeakyReLU()
-        self.ec7 = nn.Conv2d(1024, 2048, 3, stride=2, padding=1)
+        self.ec7 = nn.Conv2d(1024, 2048, 3, stride=2, padding=1, dtype=torch.float16)
         self.ec7A = nn.LeakyReLU()
-        self.ec8 = nn.Conv2d(2048, 4096, 3, stride=2, padding=1)
+        self.ec8 = nn.Conv2d(2048, 4096, 3, stride=2, padding=1, dtype=torch.float16)
         self.ec8A = nn.LeakyReLU()
-        self.ec9 = nn.Conv2d(4096, 8192, 3, stride=2, padding=1)
+        self.ec9 = nn.Conv2d(4096, 8192, 3, stride=2, padding=1, dtype=torch.float16)
         self.ec9A = nn.LeakyReLU()
 
         self.encFlatten = nn.Flatten(start_dim=1)
 
-        self.enc_mu = nn.Linear(8192, hidden_dim)
-        self.enc_var = nn.Linear(8192, hidden_dim)
+        self.enc_mu = nn.Linear(8192, hidden_dim, dtype=torch.float16)
+        self.enc_var = nn.Linear(8192, hidden_dim, dtype=torch.float16)
 
-        self.decoder_in = nn.Linear(hidden_dim, 8192)
-        self.dc1 = nn.ConvTranspose2d(8192, 4096, 3, stride=2, padding=1, output_padding=1)
+        self.decoder_in = nn.Linear(hidden_dim, 8192, dtype=torch.float16)
+        self.dc1 = nn.ConvTranspose2d(8192, 4096, 3, stride=2, padding=1, output_padding=1, dtype=torch.float16)
         self.dc1A = nn.LeakyReLU()
-        self.dc2 = nn.ConvTranspose2d(4096, 2048, 3, stride=2, padding=1, output_padding=1)
+        self.dc2 = nn.ConvTranspose2d(4096, 2048, 3, stride=2, padding=1, output_padding=1, dtype=torch.float16)
         self.dc2A = nn.LeakyReLU()
-        self.dc3 = nn.ConvTranspose2d(2048, 1024, 3, stride=2, padding=1, output_padding=1)
+        self.dc3 = nn.ConvTranspose2d(2048, 1024, 3, stride=2, padding=1, output_padding=1, dtype=torch.float16)
         self.dc3A = nn.LeakyReLU()
-        self.dc4 = nn.ConvTranspose2d(1024, 512, 3, stride=2, padding=1, output_padding=1)
+        self.dc4 = nn.ConvTranspose2d(1024, 512, 3, stride=2, padding=1, output_padding=1, dtype=torch.float16)
         self.dc4A = nn.LeakyReLU()
-        self.dc5 = nn.ConvTranspose2d(512, 256, 3, stride=2, padding=1, output_padding=1)
+        self.dc5 = nn.ConvTranspose2d(512, 256, 3, stride=2, padding=1, output_padding=1, dtype=torch.float16)
         self.dc5A = nn.LeakyReLU()
-        self.dc6 = nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1)
+        self.dc6 = nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1, dtype=torch.float16)
         self.dc6A = nn.LeakyReLU()
-        self.dc7 = nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1)
+        self.dc7 = nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1, dtype=torch.float16)
         self.dc7A = nn.LeakyReLU()
-        self.dc8 = nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1)
+        self.dc8 = nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1, dtype=torch.float16)
         self.dc8A = nn.LeakyReLU()
-        self.dc9 = nn.ConvTranspose2d(32, startCh, 3, stride=2, padding=1, output_padding=1)
+        self.dc9 = nn.ConvTranspose2d(32, startCh, 3, stride=2, padding=1, output_padding=1, dtype=torch.float16)
         self.dc9A = nn.LeakyReLU()
 
 
@@ -92,7 +93,7 @@ class Vae(nn.Module):
 
     def decode(self, z):
         z = self.decoder_in(z)
-        z = z.view(1, self.encOutChannels, 1, 1)
+        z = z.view(batch_size, self.encOutChannels, 1, 1)
         z = self.dc1A(self.dc1(z))
         z = self.dc2A(self.dc2(z))
         z = self.dc3A(self.dc3(z))
@@ -111,11 +112,12 @@ class Vae(nn.Module):
 
 
 model = Vae().to(device)
-optim = torch.optim.Adam(model.parameters())
-loader = torch.utils.data.DataLoader(myDataSet(path_to_photo), shuffle=True)
+optim = torch.optim.SGD(model.parameters())
+loader = torch.utils.data.DataLoader(myDataSet(path_to_photo), shuffle=True, batch_size=batch_size)
 logger = tensorboard.SummaryWriter(log_dir='logging')
 
-def myLoss(myx, x, mu, var):
+
+def getLoss(myx, x, mu, var):
     RecLoss = nn.functional.mse_loss(myx, x)
 
     if is_my_KLD:
@@ -126,16 +128,22 @@ def myLoss(myx, x, mu, var):
     return RecLoss + DKL
 
 
+
 def train(epoch):
 
     sum_losses: Tensor = torch.zeros(1)
+    it = 0
     for img in loader:
+
+        print(f'img num = {it*batch_size} - {(it + 1)*batch_size - 1}')
+        it += 1
         optim.zero_grad()
         myImg, mu, var = model.forward(img)
-        losses = myLoss(myImg, img, mu, var)
+        losses = getLoss(myImg, img, mu, var)
         sum_losses += losses
         losses.backward()
         optim.step()
+
 
     logger.add_scalar('Loss', sum_losses, epoch)
 
