2116af6aa36acd3ee9c2b747ffea458405565d93
“Dmitriy <d.demidov@g.nsu.ru>
2025-04-17 01:37:57 +0700

    Fixes
    Changed the way of getting unique id for image, implemented API request for text recognition

--- detectionServer.py
+++ detectionServer.py
@@ -1,10 +1,12 @@
 import os
+import uuid
+from time import sleep
+
 import cv2
 import detect
-import hashlib
 import datetime
+import multiprocessing
 import requests
-import subprocess
 
 PATH_TO_MEDIAMTX = "D:/prg/pycharm/projects/zmeyuka/mediamtx/mediamtx.exe"
 PATH_TO_STREAMING_FILE = "D:/prg/pycharm/projects/zmeyuka/yolov5/ts_streams/cars_test.ts"
@@ -12,25 +14,35 @@ PATH_TO_WEIGHTS = "D:/prg/pycharm/projects/zmeyuka/yolov5/saved_models/processor
 PATH_TO_ORIGINS = "D:/prg/zalupen/origins"
 PATH_TO_CROPS = "D:/prg/zalupen/crops"
 CAMERA_URL = 'rtsp://localhost:8554/mystream'
-#ffmpeg -re -stream_loop -1 -i cars_test.ts -c copy -f rtsp -rtsp_transport tcp rtsp://localhost:8554/mystream
+# ffmpeg -re -stream_loop -1 -i cars_test.ts -c copy -f rtsp -rtsp_transport tcp rtsp://localhost:8554/mystream
 
-img_names = dict() # 1 - processed, 0 - not
+img_names = dict()  # 1 - processed, 0 - not
 
-def run_command(cmd):
-    result = subprocess.run(cmd)
-    return result.stdout + result.stderr
-    # os.system(cmd)
-    #subprocess.Popen(["cat"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-def start_rtsp_port_simulation(path_to_mediamtx):
-    cmd = f"{path_to_mediamtx}"
-    result_output = run_command(cmd)
-    print(f"RTSP port start result: {result_output}")
+def start_camera_simualtion(path_to_media_mtx, path_to_streaming_file):
+    os.system(f"start {path_to_media_mtx}")
+    sleep(1)
+    os.system(f"start ffmpeg "
+              f"-re -stream_loop "
+              f"-1 -i {path_to_streaming_file} -c copy "
+              f"-f rtsp -rtsp_transport tcp "
+              f"rtsp://localhost:8554/mystream"
+              )
 
-def start_camera_simulation(streaming_file):
-    cmd = f"ffmpeg -re -stream_loop -1 -i {streaming_file} -c copy -f rtsp -rtsp_transport tcp rtsp://localhost:8554/mystream"
-    result_output = run_command(cmd)
-    print(f"Camera simulation start result: {result_output}")
+
+def get_license_plate_text(path_to_license_plate, json_with_plate_text):
+    os.system(f"curl "
+              f"-X POST http://127.0.0.1:5000/ScreenTranslatorAPI/detect "
+              f"-H accept: application/json "
+              f"-H Content-Type: multipart/form-data "
+              f"-F file=@{path_to_license_plate};type=image/jpeg"
+              f"-o {os.path.splitext(os.path.basename(path_to_license_plate))[0]}"
+              f"{json_with_plate_text}.json"
+              )
+
+def post_to_result_server(path_to_image):
+
+
 
 def camera_handling(camera_url):
     cap = cv2.VideoCapture(camera_url)
@@ -41,26 +53,41 @@ def camera_handling(camera_url):
     print(f'Width: {width_org} | Height: {height_org} | FPS: {fps}')
 
     while True:
-        ret, camera_frame = cap.read()
+        ret, camera_frame = cap.read()  # blocking operation
         # img = cv2.resize(img, (1280, 720))
-        current_time = str(datetime.datetime.now()).replace("-", "_").replace(" ", "_").replace(":", "_").replace(".",
-                                                                                                                  "_")
-        os_random = hashlib.sha256(str(os.urandom(32)).encode())
-        os_random_int = str(int.from_bytes(os_random.digest(), byteorder="big"))[:20]
+
+        current_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        os_random_int = uuid.uuid4()
         camera_name = (camera_url.split("/"))[-1]
         img_name = f"{PATH_TO_ORIGINS}/{camera_name}__{current_time}__{os_random_int}.jpg"
         img_names[img_name] = 0
+
         cv2.imwrite(img_name, camera_frame)
         crop_folder_name = f"{camera_name}__{current_time}__{os_random_int}"
-        detect.run(source=img_name, weights=PATH_TO_WEIGHTS, project=PATH_TO_CROPS, save_crop=True,
-                   name=crop_folder_name)
+
+        detect.run(source=img_name,
+                   weights=PATH_TO_WEIGHTS,
+                   project=PATH_TO_CROPS,
+                   save_crop=True,
+                   name=crop_folder_name
+                   )
         img_names[img_name] = 1
         cv2.imshow("RTSP camera", camera_frame)
         cv2.waitKey(1)
 
-        frame_with_detection_name = f"{PATH_TO_CROPS}"
+        plates_folder = f"{PATH_TO_CROPS}/{crop_folder_name}/license plate"
+        plates_images = [f for f in os.listdir(plates_folder) if os.path.isfile(os.path.join(plates_folder, f))]
+
+        for plate_image in plates_images:
+            img_name, img_extension = os.path.splitext(plate_image)
+            json_with_plate_text = f"{img_name}.json"
+            get_plate_routine = multiprocessing.Process(target=get_license_plate_text,
+                                                        args=(plate_image, json_with_plate_text,))
+            get_plate_routine.start()
+            get_plate_routine.join()
+
 
 if __name__ == "__main__":
-    start_rtsp_port_simulation(PATH_TO_MEDIAMTX)
-    start_camera_simulation(PATH_TO_STREAMING_FILE)
+    start_camera_simualtion(PATH_TO_MEDIAMTX, PATH_TO_STREAMING_FILE)
+    sleep(1)
     camera_handling(CAMERA_URL)
