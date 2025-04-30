bb04560f3c4aaea171c4ce581936ad4ca78679c2
“Dmitriy <d.demidov@g.nsu.ru>
2025-05-01 04:36:44 +0700

    Changed the way of finding corresponded car label for plate
    Centroid distance is also measured now

--- detectionServer.py
+++ detectionServer.py
@@ -6,6 +6,7 @@ import cv2
 import detect_configured
 import datetime
 import multiprocessing
+import math
 import json
 import requests
 
@@ -33,7 +34,86 @@ img_names = dict()  # 1 - processed, 0 - not
 
 crossed_endpoint = dict()  # [0 - none, 1 - first, 2 - second ; crossed timecode ; speed]
 
-MAX_BOUNDS_DIFFERENCE = 20
+MAX_BOUNDS_DIFFERENCE = 5
+
+
+def plate_inside_car(plate_coords, car_coords):
+    if (plate_coords[0] >= car_coords[0] or abs(
+            plate_coords[0] - car_coords[0]) < MAX_BOUNDS_DIFFERENCE) and (
+            plate_coords[2] <= car_coords[2] or abs(
+        plate_coords[2] - car_coords[2]) < MAX_BOUNDS_DIFFERENCE) and (
+            plate_coords[1] >= car_coords[1] or abs(
+        plate_coords[1] - car_coords[1]) < MAX_BOUNDS_DIFFERENCE) and (
+            plate_coords[3] <= car_coords[3] or abs(
+        plate_coords[3] - car_coords[3]) < MAX_BOUNDS_DIFFERENCE):
+        return True
+    return False
+
+
+def calc_distance(plate_coords, car_coords):
+    x11, y11, x12, y12 = plate_coords
+    x21, y21, x22, y22 = car_coords
+
+    mean_x1 = (x11 + x12) / 2
+    mean_y1 = (y11 + y12) / 2
+
+    mean_x2 = (x21 + x22) / 2
+    mean_y2 = (y21 + y22) / 2
+    return math.sqrt((mean_x2 - mean_x1) ** 2 + (mean_y2 - mean_y1) ** 2)
+
+
+def calc_speed(plate_coords, extracted_text, current_timecode):
+    current_car_speed = "undefined"
+    mean_x = (plate_coords[0] + plate_coords[2]) / 2
+    mean_y = (plate_coords[1] + plate_coords[3]) / 2
+
+    if crossed_endpoint.get(extracted_text, 0)[0] == 0:
+        if mean_x >= FIRST_ENDPOINT_X and mean_y >= FIRST_ENDPOINT_Y:
+            crossed_endpoint[extracted_text] = [1, current_timecode, current_car_speed]
+    if mean_x >= SECOND_ENDPOINT_X and mean_y >= SECOND_ENDPOINT_Y:
+        if crossed_endpoint.get(extracted_text, 0)[0] != 1:
+            print(f"Unable to calculate speed for {extracted_text}")
+        else:
+            if crossed_endpoint[extracted_text][1] == current_timecode:
+                crossed_endpoint[extracted_text][2] = -1
+            else:
+                crossed_endpoint[extracted_text][0] = 2
+                current_car_speed = DISTANCE_BETWEEN_ENDPOINTS / (
+                            (current_timecode - crossed_endpoint[extracted_text][1]) * 3.6)
+    crossed_endpoint[extracted_text][2] = current_car_speed
+
+
+def find_nearest_centroid(plate_name, cars_labels, cars_folder):
+    corresponding_car_image = ""
+    plate_label = f"{plate_name}.txt"
+    with open(plate_label, "r") as file:
+        plate_coords = [int(coord) for coord in file.readline().split()]
+
+    min_distance_value = -1
+
+    for car_label in cars_labels:
+        car_name, img_extension = os.path.splitext(car_label)
+        car_name = f"{cars_folder}/{car_name}"
+        car_label = f"{car_name}.txt"
+        car_img = car_label.replace("txt", "jpg")
+        with open(car_label, "r") as file:
+            car_coords = [int(coord) for coord in file.readline().split()]
+        if plate_inside_car(plate_coords, car_coords) == True:
+            if min_distance_value == -1:
+                min_distance_value = calc_distance(plate_coords, car_coords)
+                corresponding_car_image = car_img
+            else:
+                temp = calc_distance(plate_coords, car_coords)
+                if temp < min_distance_value:
+                    min_distance_value = temp
+                    corresponding_car_image = car_img
+
+    corresponding_car_label = corresponding_car_image.replace("jpg", "txt")
+
+    with open(corresponding_car_label, "r") as file:
+        corresponding_car_coords = [int(coord) for coord in file.readline().split()]
+
+    return corresponding_car_image, corresponding_car_coords
 
 
 def start_camera_simualtion(path_to_media_mtx, path_to_streaming_file):
@@ -72,7 +152,7 @@ def post_to_result_server(path_to_image, json_with_plate_text, current_time):
     return requests.post("http://127.0.0.1:50505/savephoto", files=files, data=data)
 
 
-def camera_handling(camera_url):
+def start_footage_processing(camera_url):
     # cap = cv2.VideoCapture(camera_url)
     cap = cv2.VideoCapture("cars.mp4")
 
@@ -141,7 +221,7 @@ def camera_handling(camera_url):
             # get_plate_routine.start()
             # get_plate_routine.join()
             plate_response_body = get_license_plate_text(plate_img_path, json_with_plate_text).json()
-            extracted_text = plate_response_body["Detected text"]
+            extracted_text = plate_response_body["Detected text"].lower()
             print(f"\n\n\n {type(extracted_text), extracted_text, plate_name} \n\n\n")
             with open(json_with_plate_text, "w") as file:
                 file.write(f"{{\n\t\"detected_text\": \"{extracted_text}\"\n}}")
@@ -157,63 +237,30 @@ def camera_handling(camera_url):
             plate_label = f"{plate_name}.txt"
             with open(plate_label, "r") as file:
                 plate_coords = [int(coord) for coord in file.readline().split()]
-            plate_inside_car = False
-            current_car_speed = "undefined"
-            for car_label in cars_labels:
-                car_name, img_extension = os.path.splitext(car_label)
-                car_name = f"{cars_folder}/{car_name}"
-                car_label = f"{car_name}.txt"
-                with open(car_label, "r") as file:
-                    car_coords = [int(coord) for coord in file.readline().split()]
+            calc_speed(plate_coords, extracted_text, current_timecode)
+            corresponding_car_image, corresponding_car_coords = find_nearest_centroid(plate_name, cars_labels,
+                                                                                      cars_folder)
 
-                    mean_x = (plate_coords[0] + plate_coords[2]) / 2
-                    mean_y = (plate_coords[1] + plate_coords[3]) / 2
-
-                    if crossed_endpoint.get(extracted_text, 0)[0] == 0:
-                        if mean_x >= FIRST_ENDPOINT_X and mean_y >= FIRST_ENDPOINT_Y:
-                            crossed_endpoint[extracted_text] = [1, current_timecode, current_car_speed]
-                    if mean_x >= SECOND_ENDPOINT_X and mean_y >= SECOND_ENDPOINT_Y:
-                        if crossed_endpoint.get(extracted_text, 0)[0] != 1:
-                            print(f"Unable to calculate speed for {extracted_text}")
-                        else:
-                            if crossed_endpoint[extracted_text][1] == current_timecode:
-                                crossed_endpoint[extracted_text][2] = -1
-                            else:
-                                crossed_endpoint[extracted_text][0] = 2
-                                current_car_speed = current_timecode - crossed_endpoint[extracted_text][1]
-                    crossed_endpoint[extracted_text][2] = current_car_speed
-                    if (plate_coords[0] >= car_coords[0] or abs(
-                            plate_coords[0] - car_coords[0]) < MAX_BOUNDS_DIFFERENCE) and (
-                            plate_coords[2] <= car_coords[2] or abs(
-                        plate_coords[2] - car_coords[2]) < MAX_BOUNDS_DIFFERENCE) and (
-                            plate_coords[1] >= car_coords[1] or abs(
-                        plate_coords[1] - car_coords[1]) < MAX_BOUNDS_DIFFERENCE) and (
-                            plate_coords[3] <= car_coords[3] or abs(
-                        plate_coords[3] - car_coords[3]) < MAX_BOUNDS_DIFFERENCE):
-                        plate_inside_car = True
-                    if plate_inside_car == True:
-                        corresponding_car_image = car_label.replace("txt", "jpg")
-                        break
-            if plate_inside_car == False:
+            if corresponding_car_image == "":
                 print(f"No car for plate {plate_name} with coordinates {plate_coords} was found")
                 continue
-            post_result = post_to_result_server(corresponding_car_image, json_with_plate_text, current_time)
-            print(post_result)
+            # post_result = post_to_result_server(corresponding_car_image, json_with_plate_text, current_time)
+            # print(post_result)
             if crossed_endpoint[extracted_text][2] != "undefined" and crossed_endpoint[extracted_text][2] < 0:
                 print(
                     f"Unexpected speed '{crossed_endpoint[extracted_text][2]}' of car with license plate {extracted_text}")
             cv2.putText(img_with_detection,
                         f"License plate: {extracted_text}",
-                        (int(car_coords[0] + 10), int(car_coords[1] + 40)),
+                        (int(corresponding_car_coords[0] + 10), int(corresponding_car_coords[1] + 45)),
                         cv2.FONT_HERSHEY_DUPLEX,
-                        1,
+                        2,
                         (0, 0, 255),
                         2)
             cv2.putText(img_with_detection,
                         f"Speed: {str(crossed_endpoint[extracted_text][2])}",
-                        (int(car_coords[0] + 10), int(car_coords[1] + 90)),
+                        (int(corresponding_car_coords[0] + 10), int(corresponding_car_coords[1] + 95)),
                         cv2.FONT_HERSHEY_DUPLEX,
-                        1,
+                        2,
                         (0, 0, 255),
                         2)
         img_with_detection = cv2.resize(img_with_detection, (1280, 720))
@@ -221,12 +268,14 @@ def camera_handling(camera_url):
         cv2.waitKey(1)
     cap.release()
 
+
 def main():
-    start_camera_simualtion(PATH_TO_MEDIAMTX, PATH_TO_STREAMING_FILE)
-    start_result_server(PATH_TO_RESULT_SERVER)
+    # start_camera_simualtion(PATH_TO_MEDIAMTX, PATH_TO_STREAMING_FILE)
+    # start_result_server(PATH_TO_RESULT_SERVER)
     start_text_extraction_server(PATH_TO_TEXT_EXTRACTION_SERVER)
     sleep(1)
-    camera_handling(CAMERA_URL)
+    start_footage_processing(CAMERA_URL)
+
 
 if __name__ == "__main__":
     main()
