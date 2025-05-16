30d8e5cef4975af15a64803f396c2a3501a29c48
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-05-16 18:21:01 +0700

    Vae
    Layers

--- VAE/main.py
+++ VAE/main.py
@@ -3,17 +3,22 @@ import torch.optim
 import torch.utils.tensorboard as tensorboard
 from torch import Tensor
 import matplotlib.pyplot as plt
+import torchvision
 
 from myDataLoader import myDataSet
 
-startCh = 3
-hidden_dim = 64
-cnt_epochs = 20
-path_to_photo = "D:\\Documents\\Pycharm Projects\\Vae2\\cars\\cars"
-path_to_save = 'D:\\Documents\\Pycharm Projects\\Vae2'
-name_of_model = 'model1'
-is_my_KLD = False
-batch_size = 1
+
+'''Image.fromarray from PIL, more Linear between 8192 to 64, torchvision.transforms.v2.ToImage'''
+
+
+startCh = 1
+hidden_dim = 128
+cnt_epochs = 40
+path_to_photo = "D:\\Documents\\PycharmProjects\\Vae2\\cars\\cars"
+path_to_save = 'D:\\Documents\\PycharmProjects\\Vae2\\model2'
+name_of_model = 'model2'
+is_my_KLD = True
+batch_size = 128
 
 
 
@@ -29,7 +34,7 @@ class Vae(nn.Module):
 
 
 
-        self.ec1 = nn.Conv2d(3, 32, 3, stride=2, padding=1)
+        self.ec1 = nn.Conv2d(startCh, 32, 3, stride=2, padding=1)
         self.ec1A = nn.LeakyReLU()
         self.ec2 = nn.Conv2d(32, 64, 3, stride=2, padding=1)
         self.ec2A = nn.LeakyReLU()
@@ -50,10 +55,34 @@ class Vae(nn.Module):
 
         self.encFlatten = nn.Flatten(start_dim=1)
 
-        self.enc_mu = nn.Linear(8192, hidden_dim)
-        self.enc_var = nn.Linear(8192, hidden_dim)
+        self.encL1 = nn.Linear(8192, 4096)
+        self.encL1A = nn.LeakyReLU()
+        self.encL2 = nn.Linear(4096, 2048)
+        self.encL2A = nn.LeakyReLU()
+        self.encL3 = nn.Linear(2048, 1024)
+        self.encL3A = nn.LeakyReLU()
+        self.encL4 = nn.Linear(1024, 512)
+        self.encL4A = nn.LeakyReLU()
+        self.encL5 = nn.Linear(512, 256)
+        self.encL5A = nn.LeakyReLU()
 
-        self.decoder_in = nn.Linear(hidden_dim, 8192)
+        self.enc_mu = nn.Linear(256, hidden_dim)
+        self.enc_var = nn.Linear(256, hidden_dim)
+
+        self.decoder_in = nn.Linear(hidden_dim, 256)
+        self.decoder_inA = nn.LeakyReLU()
+        self.decL1 = nn.Linear(256, 512)
+        self.decL1A = nn.LeakyReLU()
+        self.decL2 = nn.Linear(512, 1024)
+        self.decL2A = nn.LeakyReLU()
+        self.decL3 = nn.Linear(1024, 2048)
+        self.decL3A = nn.LeakyReLU()
+        self.decL4 = nn.Linear(2048, 4096)
+        self.decL4A = nn.LeakyReLU()
+        self.decL5 = nn.Linear(4096, 8192)
+        self.decL5A = nn.LeakyReLU()
+
+
         self.dc1 = nn.ConvTranspose2d(8192, 4096, 3, stride=2, padding=1, output_padding=1)
         self.dc1A = nn.LeakyReLU()
         self.dc2 = nn.ConvTranspose2d(4096, 2048, 3, stride=2, padding=1, output_padding=1)
@@ -71,7 +100,6 @@ class Vae(nn.Module):
         self.dc8 = nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1)
         self.dc8A = nn.LeakyReLU()
         self.dc9 = nn.ConvTranspose2d(32, startCh, 3, stride=2, padding=1, output_padding=1)
-        self.dc9A = nn.LeakyReLU()
 
 
 
@@ -86,6 +114,11 @@ class Vae(nn.Module):
         x = self.ec8A(self.ec8(x))
         x = self.ec9A(self.ec9(x))
         x = self.encFlatten(x)
+        x = self.encL1A(self.encL1(x))
+        x = self.encL2A(self.encL2(x))
+        x = self.encL3A(self.encL3(x))
+        x = self.encL4A(self.encL4(x))
+        x = self.encL5A(self.encL5(x))
         return self.enc_mu(x), self.enc_var(x)
 
     def reparam(self, mu, var):
@@ -94,7 +127,12 @@ class Vae(nn.Module):
         return mu + eps * std
 
     def decode(self, z):
-        z = self.decoder_in(z)
+        z = self.decoder_inA(self.decoder_in(z))
+        z = self.decL1A(self.decL1(z))
+        z = self.decL2A(self.decL2(z))
+        z = self.decL3A(self.decL3(z))
+        z = self.decL4A(self.decL4(z))
+        z = self.encL5A(self.decL5(z))
         z = z.view(batch_size, self.encOutChannels, 1, 1)
         z = self.dc1A(self.dc1(z))
         z = self.dc2A(self.dc2(z))
@@ -104,8 +142,8 @@ class Vae(nn.Module):
         z = self.dc6A(self.dc6(z))
         z = self.dc7A(self.dc7(z))
         z = self.dc8A(self.dc8(z))
-        z = self.dc9A(self.dc9(z))
-        return z
+
+        return self.dc9(z)
 
 
     def forward(self, x):
@@ -119,10 +157,11 @@ optim = torch.optim.SGD(model.parameters())
 loader = torch.utils.data.DataLoader(myDataSet(path_to_photo), shuffle=True, batch_size=batch_size)
 logger = tensorboard.SummaryWriter(log_dir='logging')
 scalar = torch.amp.GradScaler()
+tr = torchvision.transforms.Resize(size=(512, 512))
 
 
 def getLoss(myx, x, mu, var):
-    RecLoss = nn.functional.mse_loss(myx, x)
+    RecLoss = nn.functional.binary_cross_entropy_with_logits(myx, x, reduction="sum")
 
     if is_my_KLD:
         DKL = -0.5 * torch.sum(1 + var - mu.pow(2) - var.exp())
@@ -157,9 +196,15 @@ def train(epoch):
 
 
 def watchImg():
+    global batch_size
+    old_batch_size = batch_size
+    batch_size = 1
+
     img = loader.dataset.__getitem__(0)
     with torch.amp.autocast(device_type=device, dtype=torch.float16):
-        myImg, mu, var = model.forward(img.view(1, 3, 512, 512))
+        myImg, mu, var = model.forward(img)
+
+    myImg = torch.sigmoid(myImg)
 
     img = img.cpu() * 256
     img = img.to(torch.uint8)
@@ -169,9 +214,63 @@ def watchImg():
     plt.imshow(torch.permute(myImg * 256, (1, 2, 0)).numpy())
     plt.show()
 
+    batch_size = old_batch_size
+
 def loadModel(pathToModel):
     model.load_state_dict(torch.load(pathToModel, weights_only=True))
+
+
+def getImgInFormat(pathToImg):
+    img = torchvision.io.read_image(pathToImg)
+    img = tr(img)
+    img = img.to(torch.float16)
+
+    img_min, img_max = img.min(), img.max()
+    img = (img - img_min) / (img_max - img_min)
+
+    img = img.to(device)
+    return img.unsqueeze(0), (img_min, img_max)
+
+def encodeImg(pathToImg):
+    global batch_size
+    batch_size_prev = batch_size
+    batch_size = 1
+    img, img_info = getImgInFormat(pathToImg)
+
+    with torch.amp.autocast(device_type=device, dtype=torch.float16):
+        mu, var = model.encode(img)
+
+    batch_size = batch_size_prev
+
+    return model.reparam(mu, var), img_info
+
+
+
+def decodeImg(encodedImg, img_info: (Tensor, Tensor)):
+    global batch_size
+    batch_size_prev = batch_size
+    batch_size = 1
+
+    with torch.amp.autocast(device_type=device, dtype=torch.float16):
+        img = model.decode(encodedImg)
+        img = torch.sigmoid(img)
+
+    batch_size = batch_size_prev
+
+    img = img * (img_info[1] - img_info[0])  + img_info[0]
+
+    return img
+
+def showForwardedImg(pathToImg):
     model.eval()
+    myImg, info = encodeImg(pathToImg)
+
+    myImg = decodeImg(myImg, img_info=info)
+
+    myImg = myImg.to(torch.uint8)
+    plt.imshow(torch.permute(myImg, (1, 2, 0)).numpy())
+
+
 
 def mainTrain(epochs):
     model.train()
@@ -182,5 +281,5 @@ def mainTrain(epochs):
 
 
 if __name__ == "__main__":
-    loadModel('D:\\Documents\\Pycharm Projects\\Vae2\\model1Epoch19.pt')
-    watchImg()
+    print(f'device == {device}\n')
+    mainTrain(cnt_epochs)
--- VAE/myDataLoader.py
+++ VAE/myDataLoader.py
@@ -9,6 +9,7 @@ class myDataSet(Dataset):
     def __init__(self, dir):
         self.dir = dir
         self.tr = torchvision.transforms.Resize(size=(512, 512))
+        self.toGrey = torchvision.transforms.Grayscale(num_output_channels=1)
         self.images = os.listdir(self.dir)
 
     def __len__(self):
@@ -17,10 +18,12 @@ class myDataSet(Dataset):
     def __getitem__(self, index):
 
         img = torchvision.io.read_image(self.dir + '\\' + self.images[index])
+        img = self.toGrey(img)
         img = self.tr(img)
         img = img.to(torch.float16)
 
-        img = img / 256
+        img_min, img_max = img.min(), img.max()
+        img = (img - img_min) / (img_max - img_min)
 
         img = img.to(device)
         return img
