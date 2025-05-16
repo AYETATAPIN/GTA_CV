11fe3ffca6852adc96ec2f99cba2cf5b962c1555
“Dmitriy <d.demidov@g.nsu.ru>
2025-05-16 12:02:44 +0700

    Getting close to completion
    changed the plate text detection API call
    changed the way of sending plate images (all combined in one instead of every single one separately)

--- detectionServer.py
+++ detectionServer.py
@@ -1,14 +1,13 @@
 import os
 import uuid
-from time import sleep
-
 import cv2
 import detect_configured
 import datetime
-import multiprocessing
 import math
 import json
 import requests
+
+from combine_plates import combine_plates
 
 PATH_TO_RESULT_SERVER = "D:/prg/pycharm/projects/zmeyuka/yolov5/ResultServer.py"
 PATH_TO_TEXT_EXTRACTION_SERVER = "D:/prg/pycharm/projects/zmeyuka/yolov5/Server/server.py"
@@ -30,11 +29,13 @@ SECOND_ENDPOINT_Y = 200
 
 DISTANCE_BETWEEN_ENDPOINTS = 5
 
-img_names = dict()  # 1 - processed, 0 - not
-
 crossed_endpoint = dict()  # [0 - none, 1 - first, 2 - second ; crossed timecode ; speed]
 
 MAX_BOUNDS_DIFFERENCE = 5
+
+
+def get_texts_from_letters(letters, plate_names_and_y_coords):
+    return None
 
 
 def plate_inside_car(plate_coords, car_coords):
@@ -59,6 +60,7 @@ def calc_distance(plate_coords, car_coords):
 
     mean_x2 = (x21 + x22) / 2
     mean_y2 = (y21 + y22) / 2
+
     return math.sqrt((mean_x2 - mean_x1) ** 2 + (mean_y2 - mean_y1) ** 2)
 
 
@@ -79,13 +81,14 @@ def calc_speed(plate_coords, extracted_text, current_timecode):
             else:
                 crossed_endpoint[extracted_text][0] = 2
                 current_car_speed = DISTANCE_BETWEEN_ENDPOINTS / (
-                            (current_timecode - crossed_endpoint[extracted_text][1]) * 3.6)
+                        (current_timecode - crossed_endpoint[extracted_text][1]) * 3.6)
     crossed_endpoint[extracted_text][2] = current_car_speed
 
 
 def find_nearest_centroid(plate_name, cars_labels, cars_folder):
     corresponding_car_image = ""
     plate_label = f"{plate_name}.txt"
+
     with open(plate_label, "r") as file:
         plate_coords = [int(coord) for coord in file.readline().split()]
 
@@ -96,16 +99,18 @@ def find_nearest_centroid(plate_name, cars_labels, cars_folder):
         car_name = f"{cars_folder}/{car_name}"
         car_label = f"{car_name}.txt"
         car_img = car_label.replace("txt", "jpg")
+
         with open(car_label, "r") as file:
             car_coords = [int(coord) for coord in file.readline().split()]
+
         if plate_inside_car(plate_coords, car_coords) == True:
+            current_distance = calc_distance(plate_coords, car_coords)
             if min_distance_value == -1:
-                min_distance_value = calc_distance(plate_coords, car_coords)
+                min_distance_value = current_distance
                 corresponding_car_image = car_img
             else:
-                temp = calc_distance(plate_coords, car_coords)
-                if temp < min_distance_value:
-                    min_distance_value = temp
+                if current_distance < min_distance_value:
+                    min_distance_value = current_distance
                     corresponding_car_image = car_img
 
     corresponding_car_label = corresponding_car_image.replace("jpg", "txt")
@@ -118,7 +123,6 @@ def find_nearest_centroid(plate_name, cars_labels, cars_folder):
 
 def start_camera_simualtion(path_to_media_mtx, path_to_streaming_file):
     os.system(f"start {path_to_media_mtx}")
-    sleep(1)
     os.system(f"start ffmpeg "
               f"-re -stream_loop "
               f"-1 -i {path_to_streaming_file} -c copy "
@@ -135,29 +139,31 @@ def start_text_extraction_server(path_to_extraction_server):
     os.system(f"start python {path_to_extraction_server}")
 
 
-def get_license_plate_text(path_to_license_plate, json_with_plate_text):
-    # with open(json_with_plate_text, "w") as file:  # temporarily
-    #     file.write('{\n\t"detected_text": "911GG"\n}')
+def get_license_plate_text(path_to_license_plate):
     files = {"File": open(path_to_license_plate, "rb")}
-    return requests.post("http://127.0.0.1:5000/ScreenTranslatorAPI/imageDetect", files=files)
+    return requests.post("https://sigmaltd.space/translator/", files=files)
+    # return requests.post("http://127.0.0.1:5000/ScreenTranslatorAPI/imageDetect", files=files)
 
 
 def post_to_result_server(path_to_image, json_with_plate_text, current_time):
     with open(json_with_plate_text, "r") as file:
         json_data = json.load(file)
+
     extracted_text = json_data["detected_text"]
     files = {"image": open(path_to_image, "rb")}
     data = {"license_plate": extracted_text,
             "date_time": current_time}
+
     return requests.post("http://127.0.0.1:50505/savephoto", files=files, data=data)
 
 
 def start_footage_processing(camera_url):
-    # cap = cv2.VideoCapture(camera_url)
     cap = cv2.VideoCapture("cars.mp4")
+    # cap = cv2.VideoCapture(camera_url)
 
     width_org = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height_org = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     frames_ct = 0
     print(f'Width: {width_org} | Height: {height_org} | FPS: {fps}')
@@ -166,15 +172,14 @@ def start_footage_processing(camera_url):
 
     while True:
         ret, camera_frame = cap.read()  # blocking operation
-        # img = cv2.resize(img, (1280, 720))
         frames_ct += 1
         current_timecode = frames_ct / fps
 
         current_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
         os_random_int = uuid.uuid4()
         camera_name = (camera_url.split("/"))[-1]
+
         img_name = f"{PATH_TO_ORIGINS}/{camera_name}__{current_time}__{os_random_int}.jpg"
-        img_names[img_name] = 0
 
         cv2.imwrite(img_name, camera_frame)
         crop_folder_name = f"{camera_name}__{current_time}__{os_random_int}"
@@ -188,67 +193,91 @@ def start_footage_processing(camera_url):
                               name=crop_folder_name,
                               conf_thres=0.5
                               )
-        img_names[img_name] = 1
+
         img_with_detection_name = f"{PATH_TO_CROPS}/{crop_folder_name}/{crop_folder_name}.jpg"
         img_with_detection = cv2.imread(img_with_detection_name)
-        # img_with_detection = cv2.resize(img_with_detection, (1280, 720))
-        # cv2.imshow("RTSP camera", camera_frame)
-        # cv2.imshow("RTSP camera", img_with_detection)
-        # cv2.waitKey(1)
 
         plates_folder = f"{PATH_TO_CROPS}/{crop_folder_name}/License plate"
+        cars_folder = f"{PATH_TO_CROPS}/{crop_folder_name}/car"
+
         if os.path.isdir(plates_folder) is False:
             print("No license plates detected")
             continue
+
         plates_images = [f for f in os.listdir(plates_folder) if
                          f[-3:] != "txt" and os.path.isfile(os.path.join(plates_folder, f))]
         plates_labels = [f for f in os.listdir(plates_folder) if
                          f[-3:] == "txt" and os.path.isfile(os.path.join(plates_folder, f))]
 
-        cars_folder = f"{PATH_TO_CROPS}/{crop_folder_name}/car"
         cars_images = [f for f in os.listdir(cars_folder) if
                        f[-3:] != "txt" and os.path.isfile(os.path.join(cars_folder, f))]
         cars_labels = [f for f in os.listdir(cars_folder) if
                        f[-3:] == "txt" and os.path.isfile(os.path.join(cars_folder, f))]
 
+        combined_plates_img, plate_names_and_y_coords = combine_plates(plates_folder)
+
+        if len(plate_names_and_y_coords) == 0:
+            print(combined_plates_img)
+        else:
+            plate_response_body = get_license_plate_text(combined_plates_img).json()
+            letters = []
+            letters_ct = 0
+            while True:
+                try:
+                    new_letter = []
+                    keys = []
+                    current_letter = plate_response_body[str(letters_ct)]
+                    for key in current_letter.keys():
+                        keys.append(key)
+                    if len(keys) > 1:
+                        raise Exception("More than 1 letter for 1 coordinate")
+                    new_letter.append(keys[0])
+                    new_letter.append(current_letter[keys[0]])
+                    letters.append([new_letter])
+                except Exception as E:
+                    if E.args[0] == "Missing property exception":
+                        break
+                    else:
+                        print(E)
+
+        corresponding_plates_texts = get_texts_from_letters(letters, plate_names_and_y_coords)
+
         for plate_image in plates_images:
             plate_name, img_extension = os.path.splitext(plate_image)
-            plate_img_path = f"{plates_folder}/{plate_image}"
             plate_name = f"{plates_folder}/{plate_name}"
+            plate_img_path = f"{plate_name}.jpg"
+            plate_label = f"{plate_name}.txt"
+
             json_with_plate_text = f"{plate_name}.json"
-            # get_plate_routine = multiprocessing.Process(target=get_license_plate_text,
-            #                                             args=(plate_image, json_with_plate_text,))
-            # get_plate_routine.start()
-            # get_plate_routine.join()
-            plate_response_body = get_license_plate_text(plate_img_path, json_with_plate_text).json()
-            extracted_text = plate_response_body["Detected text"].lower()
+            extracted_text = corresponding_plates_texts[plate_img_path].lower()
             print(f"\n\n\n {type(extracted_text), extracted_text, plate_name} \n\n\n")
+
             with open(json_with_plate_text, "w") as file:
                 file.write(f"{{\n\t\"detected_text\": \"{extracted_text}\"\n}}")
 
-            # with open(json_with_plate_text, "r") as file:
-            #     json_data = json.load(file)
-            # extracted_text = json_data["detected_text"
-
             if crossed_endpoint.get(extracted_text, -1) == -1:
-                # crossed_endpoint[extracted_text] = {0, 0, 0}
                 crossed_endpoint[extracted_text] = [0, 0, "undefined"]
 
-            plate_label = f"{plate_name}.txt"
+
             with open(plate_label, "r") as file:
                 plate_coords = [int(coord) for coord in file.readline().split()]
+
             calc_speed(plate_coords, extracted_text, current_timecode)
+
             corresponding_car_image, corresponding_car_coords = find_nearest_centroid(plate_name, cars_labels,
                                                                                       cars_folder)
 
             if corresponding_car_image == "":
                 print(f"No car for plate {plate_name} with coordinates {plate_coords} was found")
                 continue
-            # post_result = post_to_result_server(corresponding_car_image, json_with_plate_text, current_time)
-            # print(post_result)
+
+            post_result = post_to_result_server(corresponding_car_image, json_with_plate_text, current_time)
+            print(post_result)
+
             if crossed_endpoint[extracted_text][2] != "undefined" and crossed_endpoint[extracted_text][2] < 0:
                 print(
                     f"Unexpected speed '{crossed_endpoint[extracted_text][2]}' of car with license plate {extracted_text}")
+
             cv2.putText(img_with_detection,
                         f"License plate: {extracted_text}",
                         (int(corresponding_car_coords[0] + 10), int(corresponding_car_coords[1] + 45)),
@@ -256,6 +285,7 @@ def start_footage_processing(camera_url):
                         2,
                         (0, 0, 255),
                         2)
+
             cv2.putText(img_with_detection,
                         f"Speed: {str(crossed_endpoint[extracted_text][2])}",
                         (int(corresponding_car_coords[0] + 10), int(corresponding_car_coords[1] + 95)),
@@ -263,6 +293,7 @@ def start_footage_processing(camera_url):
                         2,
                         (0, 0, 255),
                         2)
+
         img_with_detection = cv2.resize(img_with_detection, (1280, 720))
         cv2.imshow("RTSP camera", img_with_detection)
         cv2.waitKey(1)
@@ -270,10 +301,9 @@ def start_footage_processing(camera_url):
 
 
 def main():
-    # start_camera_simualtion(PATH_TO_MEDIAMTX, PATH_TO_STREAMING_FILE)
-    # start_result_server(PATH_TO_RESULT_SERVER)
+    start_camera_simualtion(PATH_TO_MEDIAMTX, PATH_TO_STREAMING_FILE)
+    start_result_server(PATH_TO_RESULT_SERVER)
     start_text_extraction_server(PATH_TO_TEXT_EXTRACTION_SERVER)
-    sleep(1)
     start_footage_processing(CAMERA_URL)
 
 
